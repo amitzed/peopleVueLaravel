ed86e2275bcb2e66988967219b524134d213a7d5
Amit <amitzmn@gmail.com>
2019-03-25 21:00:49 -0400

    Edit in list-post page working and redirect back to list

--- resources/js/components/Edit.vue
+++ resources/js/components/Edit.vue
@@ -1,21 +1,83 @@
 <template>
-  <div class="row justify-content-center">
-      <div class="col-md-8">
-          <div class="card card-default">
-              <div class="card-header">Edit</div>
+  <div>
+    <h1>Edit Post</h1>
+    <form @submit.prevent="updatePost">
 
-              <div class="card-body">
-                  The Edit component.
-              </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>First Name:</label>
+            <input type="text" class="form-control" v-model="post.first_name">
           </div>
+        </div>
+        </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Last Name:</label>
+            <input type="text" class="form-control" v-model="post.last_name">
+          </div>
+        </div>
+        </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Age:</label>
+            <input type="number" class="form-control" v-model="post.age">
+          </div>
+        </div>
+        </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Email:</label>
+            <input type="email" class="form-control" v-model="post.email">
+          </div>
+        </div>
+        </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Secret:</label>
+            <input type="text" class="form-control" v-model="post.secret">
+          </div>
+        </div>
+        </div>
+
+      <br />
+      <div class="form-group">
+        <button class="btn btn-primary">Update</button>
       </div>
+
+    </form>
   </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Example Component mounted.')
-        }
+  export default {
+
+    data() {
+      return {
+        post: {}
+      }
+    },
+    created() {
+      let uri = `http://localhost:8000/api/post/edit/${this.$route.params.id}`;
+      this.axios.get(uri).then((response) => {
+          this.post = response.data;
+      });
+    },
+    methods: {
+      updatePost() {
+        let uri = `http://localhost:8000/api/post/update/${this.$route.params.id}`;
+        this.axios.post(uri, this.post).then((response) => {
+          this.$router.push({name: 'posts'});
+        });
+      }
     }
+  }
 </script>
--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,63 +1,3 @@
-<!-- <template id="post-list">
-  <div class="row">
-    <div class="pull-right">
-      <router-link class="btn btn-xs btn-primary">
-        <span class="glyphicon glyphicon-plus"></span>
-        Add New Data
-      </router-link>
-    </br></br>
-    </div>
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Email</th>
-          <th>Secret</th>
-          <th class="col-md-2">Update</th>
-        </tr>
-      </thead>
-       <tbody>
-         <tr v-for="(post, index) in filteredPosts">
-           <td>{{ index + 1 }}</td>
-           <td>{{ post.first_name }} {{ post.last_name }}</td>
-           <td>{{ post.age }}</td>
-           <td>{{ post.email }}</td>
-           <td>{{ post.secret }}</td>
-           <td>
-            <router-link class="btn btn-info btn-xs" v-bind:to="{name: 'Viewpost', params: {id: post.id}}">Show</router-link>
-            <router-link class="btn btn-warning btn-xs" v-bind:to="{name: 'Editpost', params: {id: post.id}}">Edit</router-link>
-            <router-link class="btn btn-danger btn-xs" v-bind:to="{name: 'Deletepost', params: {id: post.id}}">X</router-link>
-           </td>
-         </tr>
-       </tbody>
-    </table>
-  </div>
-</template>
-
-<script>
-export default {
-  data:function(){
-    return {posts: ''};
-  },
-  created: function() {
-    let uri = 'http://localhost:8000/posts/';
-    Axios.get(uri).then((response) => {
-      this.posts = response.data;
-    });
-  },
-  computed: {
-    filteredPosts: function(){
-      if(this.posts.length) {
-        return this.posts;
-      }
-    }
-  }
-}
-</script> -->
-
-
 <template>
   <div>
   <h1>Data</h1>
@@ -66,7 +6,9 @@ export default {
       <div class="col-md-2">
         <router-link :to="{ name: 'create' }" class="btn btn-primary">Create Data</router-link>
       </div>
-    </div><br />
+    </div>
+
+    <br />
 
     <table class="table table-hover">
         <thead>
@@ -76,7 +18,7 @@ export default {
             <th>Age</th>
             <th>Email</th>
             <th>Secret</th>
-            <th>Update</th>
+            <th>Actions</th>
         </tr>
         </thead>
         <tbody>
@@ -98,6 +40,14 @@ export default {
   </div>
 </template>
 
+<style media="screen">
+  .table-hover tbody tr:hover td, .table-hover tbody tr:hover th {
+  background-color: #4169e1;
+  color: #FFD300;
+  transition: ease all .2s;
+  }
+</style>
+
 <script>
   export default {
       data() {
@@ -110,6 +60,7 @@ export default {
       this.axios.get(uri).then(response => {
         this.posts = response.data.data;
       });
-    }
+    },
+
   }
 </script>
